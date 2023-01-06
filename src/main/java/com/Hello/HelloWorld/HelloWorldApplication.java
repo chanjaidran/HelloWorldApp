fecb353392e0f4667a468218cc07072f68981643
chanjaidran <95965753+chanjaidran@users.noreply.github.com>
2023-01-06 18:48:03 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/Hello/HelloWorld/HelloWorldApplication.java
+++ src/main/java/com/Hello/HelloWorld/HelloWorldApplication.java
@@ -4,7 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class HelloWorldApplication {
+	
+	@GetMapping
+	public String message(){
+		return "welcome to Jaichandran";
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
